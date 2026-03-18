eafb038fac62342d820f5c8ad9fb0d856df414a4
Andrew <andrew1007r@gmail.com>
2026-03-18 02:25:18 -0400

    slightly adjust user model
    all changes:
    - included default fields provided by better-auth in user schema
    - renamed user model to match model name given by better-auth

--- server/src/db/schemas/User.ts
+++ server/src/db/schemas/User.ts
@@ -1,19 +1,37 @@
 import { Schema } from "mongoose";
 
 export const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
     unique: true,
   },
-  name: {
+  emailVerified: {
+    type: Boolean,
+    required: true,
+    unique: true,
+    default: false
+  },
+  phone: {
     type: String,
     required: true,
     unique: true,
   },
-  phone_number: {
-    type: String,
-    required: true,
+  createdAt: {
+    type: Date,
+    require: true,
     unique: true,
+    default: Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    require: true,
+    unique: true,
+    default: Date.now()
   },
 });
