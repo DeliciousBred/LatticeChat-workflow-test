851296a2bce2c41e39db1a45a4a5514da98fe286
Andrew <andrew1007r@gmail.com>
2026-03-27 04:04:33 -0400

    add cors to api

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,19 @@ const app = express();
 const server = http.createServer(app);
 const io = createIO(server);
 
+const allowedOrigins = [
+  'http://165.245.167.192:3000',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use("/api", apiRouter);
 
