cb2503b3e2e346ecac63afbbf361f1d41dfaa0de
Andrew <andrew1007r@gmail.com>
2026-04-02 03:19:23 -0400

    create user conversation router

--- server/src/http/index.ts
+++ server/src/http/index.ts
@@ -1,11 +1,13 @@
-import auth from "../util/auth";
-import { toNodeHandler } from "better-auth/node";
-import { Router } from "express";
-import userRouter from "./routes/userRouter";
+import auth from '../util/auth';
+import { toNodeHandler } from 'better-auth/node';
+import { Router } from 'express';
+import userRouter from './routes/userRouter';
+import conversationRouter from './routes/conversationRouter';
 
 const apiRouter = Router();
 
-apiRouter.all("/auth/*any", toNodeHandler(auth));
-apiRouter.use("/users/:id", userRouter);
+apiRouter.all('/auth/*any', toNodeHandler(auth));
+apiRouter.use('/users/:user_id', userRouter);
+apiRouter.use('/conversations/:conversation_id', conversationRouter);
 
 export default apiRouter;
--- server/src/http/routes/userRouter.ts
+++ server/src/http/routes/userRouter.ts
@@ -7,18 +7,18 @@ import {
   handleRemoveFriendRequest,
 } from '../services/friendServices';
 import { handleGetBasicUserInformation } from '../services/userServices';
+import conversationRouter from './conversationRouter';
 
 const userRouter = Router({ mergeParams: true });
 
 userRouter.get('/', handleGetBasicUserInformation);
 
-userRouter.post('/friend-requests', validateUser, handleAddFriendRequest);
-userRouter.delete('/friend-requests', validateUser, handleRemoveFriendRequest);
-userRouter.delete('/friends', validateUser, handleRemoveFriend);
 userRouter.use(attachSession, validateUser);
 userRouter.get('/friend-requests', handleGetFriendRequests);
 userRouter.post('/friend-requests', handleAddFriendRequest);
 userRouter.delete('/friend-requests', handleRemoveFriendRequest);
 userRouter.delete('/friends', handleRemoveFriend);
+
+userRouter.use('/conversations/:conversation_id', conversationRouter);
 
 export default userRouter;
