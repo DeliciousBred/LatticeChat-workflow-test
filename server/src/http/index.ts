d33faccaff39987b6b567e9acd082d0187d214b4
Andrew <andrew1007r@gmail.com>
2026-04-02 22:10:43 -0400

    add routing to auth router (not better-auth)

--- server/src/http/index.ts
+++ server/src/http/index.ts
@@ -2,12 +2,12 @@ import auth from '../util/auth';
 import { toNodeHandler } from 'better-auth/node';
 import { Router } from 'express';
 import userRouter from './routes/userRouter';
-import conversationRouter from './routes/conversationRouter';
+import authRouter from './routes/authRouter';
 
 const apiRouter = Router();
 
 apiRouter.all('/auth/*any', toNodeHandler(auth));
+apiRouter.use('/auth', authRouter);
 apiRouter.use('/users/:user_id', userRouter);
-apiRouter.use('/conversations/:conversation_id', conversationRouter);
 
 export default apiRouter;
