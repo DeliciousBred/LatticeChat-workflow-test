7a0c9f911b9247356e3e817cc3fd235c48b12e8c
DeliciousBread <andrew1007r@gmail.com>
2026-04-03 13:16:13 -0400

    adjust email taken and username taken api endpoint

--- server/src/http/services/authServices.ts
+++ server/src/http/services/authServices.ts
@@ -1,24 +1,32 @@
 import { Service } from '../types';
 import { User } from '../../db/models';
+import { isEmailTaken, isUsernameTaken } from '../../db';
+import { handleHttpError } from '../../util/error';
 
 const handleEmailTaken: Service = async (req, res) => {
-  const email = req.query.email as string;
-  if (!email) {
-    res.status(400).json({ error: 'Email is required' });
-    return;
+  const email = req.body?.email ?? '';
+  
+  try {
+    const isTaken = await isEmailTaken(email);
+    res.status(200).send({
+      isTaken: isTaken,
+    });
+  } catch (error) {
+    handleHttpError(error, res);
   }
-  const user = await User.findOne({ email: email });
-  res.json({ taken: user != null });
 };
 
 const handleUsernameTaken: Service = async (req, res) => {
-  const username = req.query.username as string;
-  if (!username) {
-    res.status(400).json({ error: 'Username is required' });
-    return;
+  const username = req.body?.username ?? '';
+
+  try {
+    const isTaken = await isUsernameTaken(username);
+    res.status(200).send({
+      isTaken: isTaken,
+    });
+  } catch (error) {
+    handleHttpError(error, res);
   }
-  const user = await User.findOne({ username: username });
-  res.json({ taken: user != null });
 };
 
 export { handleEmailTaken, handleUsernameTaken };
